1914e14fbd77c1495652f9353cc2d85f74e6a22c
iLxlo <98545753+iLxlo@users.noreply.github.com>
2023-12-23 01:30:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from src.config import Config
 from src.gen import get_timestamp
 import ctypes
+import os
 
 intents = discord.Intents.default().all()
 
@@ -10,14 +11,21 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(
-        "\x1b[35m ╔═╗┬─┐┌─┐┌┬┐┌─┐  ┌─┐┌─┐┌┐┌\x1b[0m" + "\n"
-        "\x1b[35m ╠═╝├┬┘│ │││││ │  │ ┬├┤ │││\x1b[0m" + "\n"
-        "\x1b[35m ╩  ┴└─└─┘┴ ┴└─┘  └─┘└─┘┘└┘\x1b[0m" + "\n"
-        "\x1b[35m                         Made by ilxlo\x1b[0m"
-    )
-    print(f'{get_timestamp()} \x1b[32m(!)\x1b[0m Logged in as [\x1b[90m{bot.user.name}\x1b[0m]')
-    print(f'{get_timestamp()} \x1b[33m(*)\x1b[0m Invite link: https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=8&scope=bot')
+    center_lines = [
+        "\x1b[35m ╔═╗┬─┐┌─┐┌┬┐┌─┐  ┌─┐┌─┐┌┐┌\x1b[0m",
+        "\x1b[35m ╠═╝├┬┘│ │││││ │  │ ┬├┤ │││\x1b[0m",
+        "\x1b[35m ╩  ┴└─└─┘┴ ┴└─┘  └─┘└─┘┘└┘\x1b[0m",
+        "\x1b[35m                      Made by ilxlo\x1b[0m"
+    ]
+    for line in center_lines:
+        console_width = os.get_terminal_size().columns
+        center_position = (console_width - len(line)) // 2
+        print(f"{' ' * center_position}{line}")
+    print(f"{' ' * ((console_width - 35) // 2)}\x1b[32m(!)\x1b[0m {get_timestamp()} Logged in as [\x1b[90m{bot.user.name}\x1b[0m]")
+    line_count = sum(1 for line in open("./data/promos.txt"))
+    print(f"{' ' * ((console_width - 35) // 2)}\x1b[34m(/)\x1b[0m {get_timestamp()} Stock: [\x1b[90m{line_count}\x1b[0m]")
+    print(f"{' ' * ((console_width - 125) // 2)}\x1b[33m(*)\x1b[0m {get_timestamp()} Invite link: https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=8&scope=bot")
+
     ctypes.windll.kernel32.SetConsoleTitleW("Made by @ilxlo | https://github.com/iLxlo")
 
 if __name__ == "__main__":
